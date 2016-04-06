fb75ac74d5cb4b3464585198a4159b9a2886c50c
Oxygene2o3 <mro1203@hotmail.com>
2016-04-06 14:47:47 +0200

    Test

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,27 +1,27 @@
-var map;
-var direction;
-
-/* Donne les parametres par default de la map */
-function initMap() {
-    var TempoIni = new google.maps.LatLng(46.2, 6.1667); // Correspond au coordonnées d'initialisation
-    var myOptions = {
-        zoom: 14, // Zoom par défaut
-        center: TempoIni, // Coordonnées de départ
-        mapTypeId: google.maps.MapTypeId.TERRAIN, // Type de carte, différentes valeurs possible HYBRID, ROADMAP, SATELLITE, TERRAIN
-        maxZoom: 20
-    };
-    return myOptions;
-}
-
-/* initialize la map et utilise plusieurs fonctions */
-function initialize() {
-
-    map = new google.maps.Map(document.getElementById('Map'), initMap());
-
-    direction = new google.maps.DirectionsRenderer({
-        map: map
-    });
-}
+//var map;
+//var direction;
+//
+///* Donne les parametres par default de la map */
+//function initMap() {
+//    var TempoIni = new google.maps.LatLng(46.2, 6.1667); // Correspond au coordonnées d'initialisation
+//    var myOptions = {
+//        zoom: 14, // Zoom par défaut
+//        center: TempoIni, // Coordonnées de départ
+//        mapTypeId: google.maps.MapTypeId.TERRAIN, // Type de carte, différentes valeurs possible HYBRID, ROADMAP, SATELLITE, TERRAIN
+//        maxZoom: 20
+//    };
+//    return myOptions;
+//}
+//
+///* initialize la map et utilise plusieurs fonctions */
+//function initialize() {
+//
+//    map = new google.maps.Map(document.getElementById('Map'), initMap());
+//
+//    direction = new google.maps.DirectionsRenderer({
+//        map: map
+//    });
+//}
 
 function initMapParcours(nbParcours) {
 // On boucle autant de fois qu'il y a de points dans nbParcours.
@@ -85,7 +85,75 @@ function initMapParcours(nbParcours) {
     traceParcours.setMap(map);
 
 }
+function initMap()
+{
+    // Position par défaut
+    var centerpos = new google.maps.LatLng(46.2000, 6.1500);
 
+// Ansi que des options pour la carte, centrée sur latlng
+    var optionsGmaps = {
+        center: centerpos,
+        navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 15
+    };
+
+// Initialisation de la carte avec les options
+    var map = new google.maps.Map(document.getElementById("map"), optionsGmaps);
+
+    if (navigator.geolocation) {
+
+        // Fonction de callback en cas de succès
+        function affichePosition(position) {
+
+            // On instancie un nouvel objet LatLng pour Google Maps
+            var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            // Ajout d'un marqueur à la position trouvée
+            var marker = new google.maps.Marker({
+                position: latlng,
+                map: map,
+                title: "Vous êtes ici"
+            });
+
+            map.panTo(latlng);
+
+        }
+
+        // Fonction de callback en cas d’erreur
+        function erreurPosition(error) {
+            var info = "Erreur lors de la géolocalisation : ";
+            switch (error.code) {
+                case error.TIMEOUT:
+                    info += "Timeout !";
+                    break;
+                case error.PERMISSION_DENIED:
+                    info += "Vous n’avez pas donné la permission";
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    info += "La position n’a pu être déterminée";
+                    break;
+                case error.UNKNOWN_ERROR:
+                    info += "Erreur inconnue";
+                    break;
+            }
+            document.getElementById("maposition").innerHTML = info;
+        }
+
+        navigator.geolocation.getCurrentPosition(affichePosition, erreurPosition);
+
+    } else {
+
+        alert("Ce navigateur ne supporte pas la géolocalisation");
+
+    }
+}
+
+function ConfirmAndRedirect(e, t) {
+    if (confirm(e)) {
+        window.location.replace(t);
+    }
+}
 
 
 
